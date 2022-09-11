95aafc150a13714087f997651b14fd52c1f9ebba
GlowInTheDark123 <steffes.james@gmail.com>
2022-09-11 11:12:42 -0400

    first commit 	new file:   LICENSE 	new file:   Makefile 	new file:   config.h 	new file:   config.mk 	new file:   drw.c 	new file:   drw.h 	new file:   drw.o 	new file:   dwm 	new file:   dwm.1 	new file:   dwm.c 	new file:   dwm.o 	new file:   patch/dwm-stacker-6.2.diff 	new file:   patch/dwm-statuscmd-20210405-67d76bd.diff 	new file:   patch/dwm-sticky-20160911-ab9571b.diff 	new file:   patch/dwm-swallow-6.2.diff 	new file:   patch/dwm-vanitygaps-6.2.diff 	new file:   transient.c 	new file:   util.c 	new file:   util.h 	new file:   util.o 	new file:   vanitygaps.c

---
+++ config.h
@@ -0,0 +1,181 @@
+/* See LICENSE file for copyright and license details. */
+
+/* program definitions */
+#define TERMINAL "st"
+#define STATUSBAR "dwmblocks"
+
+/* appearance */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Source Code Pro:size=12:autohint=true:antialias=true", "JoyPixels:pixelsize=11" };
+static const char dmenufont[]       = "Source Code Pro:size=12:autohint=true:antialias=true";
+static const char col_gray1[]       = "#262731";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#ffffff";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#6f00be";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+/* tagging */
+static const char *tags[] = { "", "", "", "", "", "" };
+static const Rule rules[] = {
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Brave",   NULL,          NULL,           0,   	  0,          0,          -1,        -1 },
+	{ NULL,      "qemu",        NULL,           0,   	  1,          0,          1,         -1 },
+	{ NULL,      "pinentry-qt", NULL,           0,   	  1,          0,          -1,        -1 },
+	{ NULL,      "ncmpcpp",     NULL,           0,   	  1,          0,          -1,        -1 },
+	{ "St",      NULL,          NULL,           0,        0,          1,           0,        -1 },
+	{ NULL,      NULL,          "Event Tester", 0,        0,          0,           0,        -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+/* #define FORCE_VSPLIT 1 */
+#include "vanitygaps.c"
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },
+	{ "[-]",      bstack },
+
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+
+	{ "[#]",      grid },
+	{ "[M]",      monocle },
+
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ NULL,       NULL },
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+#define STACKKEYS(MOD,ACTION) \
+	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
+	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
+	{ MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
+	{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static char dmenumon[2] = "0";
+static char dmenulines[3] = "20";
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-c", "-l", dmenulines, NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
+
+#include <X11/XF86keysym.h>
+
+static Key keys[] = {
+	/* modifier                     key                 function        argument */
+	STACKKEYS(MODKEY,                          	        focus)
+	STACKKEYS(MODKEY|ShiftMask,                	        push)
+	{ MODKEY,                       XK_p,      	        spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, 	        spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_b,      	        spawn,          SHCMD("brave") },
+	{ MODKEY|ShiftMask,             XK_h,      	        spawn,          SHCMD(TERMINAL " -e htop") },
+	{ MODKEY|ShiftMask,             XK_f,      	        spawn,          SHCMD(TERMINAL " -e lf-ueberzug") },
+	{ MODKEY|ShiftMask,             XK_e,      	        spawn,          SHCMD(TERMINAL " -e nvim") },
+	{ MODKEY|ShiftMask,             XK_w,      	        spawn,          SHCMD(TERMINAL " -e nmtui") },
+	{ MODKEY|ShiftMask,             XK_m,      	        spawn,          SHCMD(TERMINAL " -e ncmpcpp-ueberzug") },
+	{ MODKEY|ShiftMask,             XK_n,      	        spawn,          SHCMD(TERMINAL " -e neomutt") },
+	{ MODKEY|ShiftMask,             XK_l,      	        spawn,          SHCMD("passmenu") },
+	{ MODKEY,                       XK_b,      	        togglebar,      {0} },
+	{ MODKEY,                       XK_i,      	        incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      	        incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      	        setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      	        setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, 	        zoom,           {0} },
+	{ MODKEY,              		    XK_z,      	        incrgaps,       {.i = +2 } },
+	{ MODKEY,    			        XK_x,      	        incrgaps,       {.i = -2 } },
+	{ MODKEY,              		    XK_a,      	        togglegaps,     {0} },
+	{ MODKEY|ShiftMask,   		    XK_a,      	        defaultgaps,    {0} },
+	{ MODKEY,                       XK_q,      	        killclient,     {0} },
+	{ MODKEY,                       XK_w,      	        spawn,          SHCMD("setbg") },
+	{ MODKEY,                       XK_t,      	        setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,             XK_t,      	        setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_y,      	        setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_y,      	        setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_u,      	        setlayout,      {.v = &layouts[4]} },
+	{ MODKEY|ShiftMask,             XK_u,      	        setlayout,      {.v = &layouts[5]} },
+	{ MODKEY,                       XK_i,      	        setlayout,      {.v = &layouts[6]} },
+	{ MODKEY|ShiftMask,             XK_i,      	        setlayout,      {.v = &layouts[7]} },
+	{ MODKEY,                       XK_space,  	        setlayout,      {0} },
+	{ MODKEY,                       XK_f,      	        togglefloating, {0} },
+	{ MODKEY,                       XK_s,      	        togglesticky,   {0} },
+	{ MODKEY|ShiftMask,		        XK_p,		        spawn,		    SHCMD("mpc toggle") },
+	{ MODKEY,			            XK_bracketleft,	    spawn,		    SHCMD("mpc seek -10") },
+	{ MODKEY|ShiftMask,		        XK_bracketleft,	    spawn,		    SHCMD("mpc seek -60") },
+	{ MODKEY,			            XK_bracketright,    spawn,		    SHCMD("mpc seek +10") },
+	{ MODKEY|ShiftMask,		        XK_bracketright,    spawn,		    SHCMD("mpc seek +60") },
+	{ MODKEY,			            XK_semicolon,	    spawn,		    SHCMD("mpc prev") },
+	{ MODKEY,			            XK_apostrophe,	    spawn,		    SHCMD("mpc next") },
+	{ MODKEY,                       XK_0,      	        view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      	        tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  	        focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, 	        focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  	        tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, 	        tagmon,         {.i = +1 } },
+	{ MODKEY,			            XK_F7,		        spawn,		    SHCMD("dspswitch") },
+	{ MODKEY,			            XK_F8,		        spawn,		    SHCMD("gamelaunch") },
+	{ MODKEY,			            XK_F9,		        spawn,		    SHCMD("mountmenu") },
+	{ MODKEY,			            XK_F10,		        spawn,		    SHCMD("umountmenu") },
+	{ 0, XF86XK_AudioMute,			                    spawn,		    SHCMD("amixer sset 'Master' toggle") },
+	{ 0, XF86XK_AudioRaiseVolume,			            spawn,		    SHCMD("amixer set 'Master' 10%+") },
+	{ 0, XF86XK_AudioLowerVolume,			            spawn,		    SHCMD("amixer set 'Master' 10%-") },
+	{ 0, XF86XK_MonBrightnessUp,			            spawn,		    SHCMD("xbacklight -inc 15") },
+	{ 0, XF86XK_MonBrightnessDown,			            spawn,		    SHCMD("xbacklight -dec 15") },
+	{ MODKEY,                       XK_Tab,    	        view,           {0} },
+	TAGKEYS(                        XK_1,                              	0)
+	TAGKEYS(                        XK_2,                      	        1)
+	TAGKEYS(                        XK_3,                      	        2)
+	TAGKEYS(                        XK_4,                      	        3)
+	TAGKEYS(                        XK_5,                      	        4)
+	TAGKEYS(                        XK_6,                      	        5)
+	{ MODKEY,			            XK_BackSpace,	    spawn,		    SHCMD("syscon") },
+	{ MODKEY|ShiftMask,		        XK_BackSpace,	    spawn,		    SHCMD("syscon") },
+	{ MODKEY|ShiftMask,             XK_q,      	        quit,           {0} },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+#ifndef __OpenBSD__
+	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
+	{ ClkStatusText,        0,              Button4,        sigdwmblocks,   {.i = 4} },
+	{ ClkStatusText,        0,              Button5,        sigdwmblocks,   {.i = 5} },
+#endif
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+};
+
